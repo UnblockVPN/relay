22e0851b2b532fef343af5242cb3311a73a9e4d0
0xCosydell <david.awatere@gmail.com>
2024-01-29 11:32:21 +0100

    Update wgEventHandler.js

--- wgEventHandler.js
+++ wgEventHandler.js
@@ -1,4 +1,5 @@
-const EventSource = require('eventsource');
+// File: wgEventHandler.js
+const { initializeSSE } = require('./sseHandler');
 const { readWgConfig } = require('./readFunctions');
 const { writeTempWgConfig, applyWgConfig } = require('./writeFunctions');
 const winston = require('winston');
@@ -20,33 +21,6 @@ const sseUrl = 'https://api.unblockvpn.io/sse/events';
 const wgConfPath = '/etc/wireguard/wg0.conf';
 const tempDir = '/home/unblockvpnio/';
 const tempConfigPath = tempDir + 'wg0.conf.update.temp';
-
-logger.debug(`Connecting to SSE URL: ${sseUrl}`);
-const eventSource = new EventSource(sseUrl);
-
-eventSource.onmessage = event => {
-    try {
-        logger.debug('Event received. Starting to process...');
-        const data = JSON.parse(event.data);
-        logger.debug(`Parsed event data: ${JSON.stringify(data, null, 2)}`);
-
-        if (data.type === 'INSERT') {
-            const { ipv4_address, pubkey } = data.data;
-            logger.debug(`Processing INSERT event for IP: ${ipv4_address} with pubkey: ${pubkey}`);
-            processInsertEvent(ipv4_address, pubkey);
-        } else if (data.type === 'DELETE') {
-            const { ipv4_address } = data.data;
-            logger.debug(`Processing DELETE event for IP: ${ipv4_address}`);
-            processDeleteEvent(ipv4_address);
-        }
-    } catch (error) {
-        logger.error(`Error processing event: ${error.message}`);
-    }
-};
-
-eventSource.onerror = error => {
-    logger.error(`EventSource encountered an error: ${JSON.stringify(error, null, 2)}`);
-};
 
 function processInsertEvent(ip, pubkey) {
     logger.debug(`Initiating processInsertEvent for IP: ${ip}`);
@@ -89,6 +63,9 @@ function removePeerFromConfig(config, ip) {
     // Make sure to return the updated configuration.
     return config; // Replace with actual removal logic.
 }
+
+// Initialize SSE
+initializeSSE(sseUrl, processInsertEvent, processDeleteEvent);
 
 // Keep the script running
 process.stdin.resume();

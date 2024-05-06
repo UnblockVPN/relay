03c8ebdf8e7500af9f52210004d2d9600b78d980
0xCosydell <david.awatere@gmail.com>
2024-05-06 13:50:03 +0200

    Update sse.js

--- sse.js
+++ sse.js
@@ -3,7 +3,7 @@ const EventSource = require('eventsource');
 const winston = require('winston');
 
 const logger = winston.createLogger({
-    level: 'info',
+    level: 'debug',
     format: winston.format.combine(
         winston.format.timestamp(),
         winston.format.json()
